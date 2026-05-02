dbbf2d23a6c0e441e3c2951ca6eec0183b77e2e2
Shubham Chaudhary <71214613+Shubhamc4@users.noreply.github.com>
2026-05-02 13:35:51 +0530

    Fix username formatting in discord_notify.sh

--- discord_notify.sh
+++ discord_notify.sh
@@ -59,7 +59,7 @@ notify_deploy() {
     --arg desc "$MSG" \
     --arg color "$COLOR" \
     '{
-      username: "`\($srv)` Bot",
+      username: "\($srv) Bot",
       embeds: [{
         title: "🚀 Automatic Deployment: \($srv)",
         description: $desc,
